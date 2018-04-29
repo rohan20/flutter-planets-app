8c73eedbae19deb5a3cb0d3c327738008a711bd1
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-29 21:40:04 +0530

    added a basic MaterialApp

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_planets_app/ui/home/HomePage.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    title: "Planets",
+    home: new HomePage(),
+  ));
+}
