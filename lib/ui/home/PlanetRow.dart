50762dcc589dbf68238968b424d7b8a96b519c02
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-30 01:38:39 +0530

    added basic PlanetRow layout

--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -4,7 +4,18 @@ class PlanetRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Container();
+    return new Container(
+      height: 120.0,
+      color: new Color(0xFF333366),
+      margin: new EdgeInsets.symmetric(
+        vertical: 16.0,
+        horizontal: 24.0
+      ),
+      child: new Stack(
+//        planetCard,
+//        planetThumbnail,
+      ),
+    );
   }
 
 }
