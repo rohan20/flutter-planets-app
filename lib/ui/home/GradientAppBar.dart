6b6ba053f7d2e422391704ee2c92febb86975e8e
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-29 21:58:16 +0530

    added BoxDecoration to GradientAppBar

--- lib/ui/home/GradientAppBar.dart
+++ lib/ui/home/GradientAppBar.dart
@@ -2,13 +2,16 @@ import 'package:flutter/material.dart';
 
 class GradientAppBar extends StatelessWidget {
 
-  var title;
+  final String title;
+  final double appBarHeight = 66.0;
 
   GradientAppBar(this.title);
 
   @override
   Widget build(BuildContext context) {
     return new Container(
+      height: appBarHeight,
+      decoration: new BoxDecoration(color: Colors.blue),
       child: new Center(
         child: new Text(title),
       ),
