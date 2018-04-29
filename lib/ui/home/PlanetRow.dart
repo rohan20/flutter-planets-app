bdbe9ff76716b89a17de8238a0b01386f054123b
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-30 01:47:45 +0530

    added planetThumbnail widget

--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -4,18 +4,25 @@ class PlanetRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final planetThumbnail = new Container(
+      margin: new EdgeInsets.symmetric(vertical: 16.0),
+      alignment: FractionalOffset.centerLeft,
+      child: new Image(
+        image: new AssetImage("assets/img/planet-mars.png"),
+        height: 92.0,
+        width: 92.0,
+      ),
+    );
+
     return new Container(
       height: 120.0,
-      color: new Color(0xFF333366),
-      margin: new EdgeInsets.symmetric(
-        vertical: 16.0,
-        horizontal: 24.0
-      ),
+      margin: new EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
       child: new Stack(
-//        planetCard,
-//        planetThumbnail,
+        children: <Widget>[
+          planetThumbnail,
+//          planetCard,
+        ],
       ),
     );
   }
-
 }
