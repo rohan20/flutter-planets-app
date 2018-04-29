790e8c738aad78d62a71918f208becb3b84efc08
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-04-30 02:04:49 +0530

    added planetCard and fixed assets folder location

--- lib/ui/home/PlanetRow.dart
+++ lib/ui/home/PlanetRow.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 class PlanetRow extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    final planetCard = new Container(
+      height: 124.0,
+      margin: new EdgeInsets.only(left: 46.0),
+      decoration: new BoxDecoration(
+          color: new Color(0xFF333366),
+          shape: BoxShape.rectangle,
+          borderRadius: new BorderRadius.circular(8.0),
+          boxShadow: <BoxShadow>[
+            new BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10.0,
+                offset: new Offset(0.0, 10.0)),
+          ]),
+    );
+
     final planetThumbnail = new Container(
       margin: new EdgeInsets.symmetric(vertical: 16.0),
       alignment: FractionalOffset.centerLeft,
@@ -19,8 +33,8 @@ class PlanetRow extends StatelessWidget {
       margin: new EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
       child: new Stack(
         children: <Widget>[
+          planetCard,
           planetThumbnail,
-//          planetCard,
         ],
       ),
     );
