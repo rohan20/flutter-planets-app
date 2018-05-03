34f737d5c738277c4ff4ff2ab30f8e4d2935209c
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-05-03 08:14:02 +0530

    added route for PlanetDetailPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_planets_app/ui/detail/PlanetsDetailPage.dart';
 import 'package:flutter_planets_app/ui/home/HomePage.dart';
 
-void main(){
+void main() {
   runApp(new MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: "Planets",
-    home: new HomePage(),
+      debugShowCheckedModeBanner: false,
+      title: "Planets",
+      home: new HomePage(),
+      routes: <String, WidgetBuilder>{
+        '/planets_detail_page': (_) => new PlanetsDetailPage(),
+      }
   ));
 }
